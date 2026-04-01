29ee0e32216bd7508b2acc78fada5ec27910f13c
dinda fitrian <dinda.fitrian4@.smk.belajar.id>
2026-04-01 09:04:23 +0700

    prbaikan

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('loginform');
+const errorMsg = document.getElementById('errorMsg');
+
+function login(event) {
+    event.preventDefault(); 
+    
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    // Password benar
+    if (user === "ndapluviophile" && pass === "phitt") {
+        localStorage.setItem("isLoggedIn", "true");
+        window.location.href = "dashboard.html"; 
+    } else {
+        // Password salah
+        errorMsg.style.display = "block"; 
+    }
+}
